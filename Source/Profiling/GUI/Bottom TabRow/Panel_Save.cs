07576e07bf59bc2c377c9e8500da439270073f40
Wiri <finley.gooding@hotmail.com>
2021-10-19 20:08:29 +1100

    - Add more gui code related to loading and saving entries from disk

--- Source/Profiling/GUI/Bottom TabRow/Panel_Save.cs
+++ Source/Profiling/GUI/Bottom TabRow/Panel_Save.cs
@@ -1,4 +1,7 @@
 ﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Reflection;
 using UnityEngine;
 using Verse;
 
@@ -12,6 +15,29 @@ namespace Analyzer.Profiling
     [HotSwappable]
     public static class Panel_Save
     {
+        private static EntryFile file = null;
+        private static EntryFile loadedFile = null;
+
+        private static FileHeader curHeader = new FileHeader()
+        {
+            MAGIC = FileUtility.ENTRY_FILE_MAGIC, // used to verify the file has not been corrupted on disk somehow.
+            scribingVer = 1,
+            targetEntries = Profiler.RECORDS_HELD,
+            name = " " // default to an empty name
+        };
+
+        private static string GetStatus()
+        {
+            if (file == null) return "Idle";
+
+            var ents = file.header.entries;
+            var target = file.header.targetEntries;
+            
+            return ents == target 
+                ? "Completed Collection" 
+                : $"Collecting Entries {ents}/{target} ({(ents / (float)target) * 100:F2}%)";
+        }
+
         public static void Draw(Rect r)
         {
             try
@@ -39,8 +65,8 @@ namespace Analyzer.Profiling
         {
             try
             {
-                Widgets.Label(r.LeftHalf(), "Status: Idle");
-                Widgets.Label(r.RightHalf(), "Previous Saved Entries: 0");
+                Widgets.Label(r.LeftHalf(), "Status: " + GetStatus());
+                Widgets.Label(r.RightHalf(), "Previous Saved Entries: " + FileUtility.PreviousEntriesFor(GUIController.CurrentProfiler.label).Count());
                 
                 throw new Exception();
             }
@@ -49,23 +75,105 @@ namespace Analyzer.Profiling
                 
             }
         }
+
+        public static void CheckValidHeader()
+        {
+            if (curHeader.MAGIC != FileUtility.ENTRY_FILE_MAGIC)
+            {
+                ThreadSafeLogger.Error($"headers magic value was {curHeader.MAGIC} not the expected {FileUtility.ENTRY_FILE_MAGIC} - correcting");
+                curHeader.MAGIC = FileUtility.ENTRY_FILE_MAGIC;
+            }
+
+            if (curHeader.entries > 0)
+            {
+                ThreadSafeLogger.Error($"headers entries value was not 0, resetting");
+                curHeader.entries = 0;
+            }
+
+            if (curHeader.name == null)
+            {
+                ThreadSafeLogger.Error("headers name value was null, should have been ' ', resetting");
+                curHeader.name = " ";
+            }
+        }
+        
         
         public static void DrawColumn(Rect r)
         {
-            try
-            {
-                if (Widgets.ButtonText(r.TopPartPixels(20), "Print Directory Information"))
-                {
-                    ThreadSafeLogger.Message(FileUtility.GetDirectory().FullName);
-                }
-                r.AdjustVerticallyBy(20f);
+            var color = GUI.color;
+            GUI.color = color * new Color(1f, 1f, 1f, 0.4f);
+            Widgets.DrawLineVertical(r.x, r.y, r.height);
+            GUI.color = color;
 
-                throw new Exception();
-            }
-            catch (Exception)
+            r = r.ContractedBy(2);
+            
+            Listing_Standard s = new Listing_Standard();
+            s.Begin(r);
+            
+            DubGUI.Heading(s, "Options");
+            s.GapLine(2f);
+            s.CheckboxLabeled("Only Entries with Values", ref curHeader.onlyEntriesWithValues);
+            s.CheckboxLabeled("One Entry per Call", ref curHeader.entryPerCall);
+            float val = curHeader.targetEntries;
+            DubGUI.LabeledSliderFloat(s, $"Target Entries : {curHeader.targetEntries}", ref val, 50, 50_000);
+            curHeader.targetEntries = Mathf.RoundToInt(val);
+
+            if (s.ButtonText("Load Settings From"))
             {
-                
+                var options = new List<FloatMenuOption>();
+                foreach (var entry in FileUtility.PreviousEntriesFor(GUIController.CurrentProfiler.label))
+                {
+                    var header = FileUtility.ReadHeader(entry);
+                    options.Add(new FloatMenuOption(header.name == "" ? entry.Name : header.name, () => curHeader = header));
+                }
+                Find.WindowStack.Add(new FloatMenu(options));
             }
+            
+            s.GapLine(12f);
+
+            if (s.ButtonText("Start Collecting Entries"))
+            {
+                curHeader.methodName = GUIController.CurrentProfiler.label;
+                
+                CheckValidHeader();
+
+                file = new EntryFile()
+                {
+                    header = curHeader,
+                    times = new float[curHeader.targetEntries],
+                    calls = new int[curHeader.targetEntries]
+                };
+            }
+
+            if (s.ButtonText("Save"))
+            {
+                FileUtility.WriteFile(null);
+            }
+
+            if (s.ButtonText("Load"))
+            {
+                var options = new List<FloatMenuOption>();
+                foreach (var entry in FileUtility.PreviousEntriesFor(GUIController.CurrentProfiler.label))
+                {
+                    var header = FileUtility.ReadHeader(entry);
+                    options.Add(new FloatMenuOption(header.name == "" ? entry.Name : header.name, () => loadedFile = FileUtility.ReadFile(entry)));
+                }
+                Find.WindowStack.Add(new FloatMenu(options));
+            }
+
+            var dbgRect = s.GetRect(Text.LineHeight * 3);
+            
+            if (Widgets.ButtonText(dbgRect.LeftHalf(),"Print directory path"))
+            {
+                ThreadSafeLogger.Message(FileUtility.GetDirectory().FullName);
+            }
+            if (Widgets.ButtonText(dbgRect.RightHalf(),"Print directory files matching current profiler"))
+            {
+                foreach(var f in FileUtility.PreviousEntriesFor(GUIController.CurrentProfiler.label))
+                    ThreadSafeLogger.Message(f.FullName);
+            }
+            
+            s.End();
         }
     }
 }
--- Source/Profiling/Utility/FileUtility.cs
+++ Source/Profiling/Utility/FileUtility.cs
@@ -4,18 +4,55 @@ using System.Collections.Generic;
 using System.IO;
 using System.Linq;
 using System.Reflection;
+using System.Text;
 using System.Text.RegularExpressions;
 using Verse;
 
 namespace Analyzer.Profiling 
 {
+    public struct FileHeader
+    {
+        // magic must equal 440985710, otherwise the file has been corrupted.
+        public int MAGIC;
+        public int scribingVer;
+        public string methodName;
+        public string name;
+        public bool entryPerCall;
+        public bool onlyEntriesWithValues;
+        public int entries;
+        public int targetEntries;
+    }
+
+    public class EntryFile
+    {
+        public FileHeader header;
+        public float[] times;
+        public int[] calls;
+    }
+    
     [HotSwappable]
     public static class FileUtility
     {
+        public const int ENTRY_FILE_MAGIC = 440985710;
+        
         private static string INVALID_CHARS = $@"[{Regex.Escape(new string(Path.GetInvalidFileNameChars()))}]+";
         public static string GetFileLocation => Path.Combine(GenFilePaths.SaveDataFolderPath, "Analyzer");
 
         public static string FinalFileNameFor(string file, int idx) => Path.Combine(GetFileLocation, file + idx + ".data");
+
+        private static FileInfo[] cachedFiles = null;
+        private static long lastFileAccess = 0;
+
+        private static void RefreshFiles()
+        {
+            var access = DateTime.Now.ToFileTimeUtc();
+
+            if (access - lastFileAccess < 15)
+            {
+                cachedFiles = GetDirectory().GetFiles();
+                lastFileAccess = access;
+            }
+        }
         
         public static DirectoryInfo GetDirectory()
         {
@@ -32,19 +69,48 @@ namespace Analyzer.Profiling
             return Regex.Replace(filename, INVALID_CHARS, "_");
         }
 
-        public static string FileNameFor(MethodInfo method)
+        public static string FileNameFor(string str)
         {
-            var sig = Utility.GetSignature(method);
-            // add suffix for number of entries already in the folder
-            return SanitizeFileName(sig);
+            // var sig = new StringBuilder(Utility.GetSignature(method, false));
+            // // name mangling
+            // sig.Append('(');
+            // foreach (var param in method.GetParameters())
+            // {
+            //     if (param.IsOut) sig.Append('o');
+            //     if (param.IsIn) sig.Append('r');
+            //     sig.Append($"{param.Name[0]}_");
+            // }
+            //
+            // sig.Append(')');
+
+            return SanitizeFileName(str);
         }
         
-        public static IEnumerable<FileInfo> PreviousEntriesFor(MethodInfo method)
+        public static IEnumerable<FileInfo> PreviousEntriesFor(string s)
         {
-            var fn = FileNameFor(method);
+            RefreshFiles();
+            
+            var fn = FileNameFor(s);
 
-            return GetDirectory().GetFiles().Where(f => f.Name.Contains(fn));
+            return cachedFiles?.Where(f => f.Name.Contains(fn)) ?? Enumerable.Empty<FileInfo>();
+        }
+        
+        public static EntryFile ReadFile(FileInfo file)
+        {
+            return null;
         }
 
+        public static void WriteFile(EntryFile file)
+        {
+            
+        }
+
+        public static FileHeader ReadHeader(FileInfo file)
+        {
+            return new FileHeader();
+        }
+        
+        
+        
     }
 }
